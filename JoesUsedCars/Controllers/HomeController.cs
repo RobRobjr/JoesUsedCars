7f9bb56a767bc1cac34362d46bb3c88d2a1b02ae
Robert Robinson <robinson6113@student.cptc.edu>
2019-04-08 10:34:03 -0700

    Created a search page and a link to it

--- JoesUsedCars/Controllers/HomeController.cs
+++ JoesUsedCars/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace JoesUsedCars.Controllers
             return View();
         }
 
+        public IActionResult Search()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
